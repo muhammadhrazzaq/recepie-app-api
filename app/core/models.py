06c617abd03d1e34743d53adc93f104b5079c06d
muhammadhrazzaq <muhammad.h.razzaq@gmail.com>
2022-09-27 07:22:26 +0100

    Add custom user model.

--- app/core/models.py
+++ app/core/models.py
@@ -7,9 +7,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves the new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
 
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """creates and save a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -26,3 +37,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+     
